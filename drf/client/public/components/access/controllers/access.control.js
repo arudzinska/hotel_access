7477b70d7b49e21c94dcf2e5354b8cda8a8a3e43
arudzinska <ania.ruiva@gmail.com>
2018-05-31 22:27:36 +0200

    clean-up

--- drf/client/public/components/access/controllers/access.control.js
+++
@@ -1,6 +0,0 @@
-access
-    .controller('AccessController', function($scope, Customer) {
-        Customer.query().$promise.then(function(data) {
-            $scope.customers = data;
-        });
-});
--- drf/serv/access/views.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import viewsets
-from access.models import Customer, Area, Rule
-from access.serializers import CustomerSerializer, AreaSerializer, RuleSerializer
-
-class CustomerViewSet(viewsets.ModelViewSet):
-    """ ViewSet for viewing and editing Customer objects """
-    queryset = Customer.objects.all()
-    serializer_class = CustomerSerializer
